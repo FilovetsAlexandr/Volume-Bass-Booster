53b26c16d803122c1d597006fa1c8e0ebf98eb10
FilovetsAlexandr <sohan.boxan@gmail.com>
2024-07-30 17:08:26 +0300

    initial commit

---
+++ Volume-Bass-Booster/Volume-Bass-Booster/Volume_Bass_BoosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volume_Bass_BoosterApp.swift
+//  Volume-Bass-Booster
+//
+//  Created by Alexandr Filovets on 30.07.24.
+//
+
+import SwiftUI
+
+@main
+struct Volume_Bass_BoosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
